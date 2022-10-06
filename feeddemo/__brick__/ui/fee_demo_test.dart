cddfd036c32506e11e8648b02d51cc48c9953d97
mac <dev@worthply.com>
2022-10-06 12:19:20 +0530

    add files

---
+++ feeddemo/__brick__/ui/fee_demo_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FeeDemoTest extends StatelessWidget {
+
+  const FeeDemoTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ feeddemo/__brick__/{{feeddemo.snakeCase()}}.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class {{feeddemo.pascalCase()}} extends StatelessWidget {
+
+  const {{feeddemo.pascalCase()}}({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
